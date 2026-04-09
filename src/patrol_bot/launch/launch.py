b222fcd14eb5bb4a7b2fe21e9066a99c3c7572c7
sharpiestealr <vicky.garcia.de.souza@gmail.com>
2026-04-08 21:52:55 -0300

    okay potentially aps2

--- src/patrol_bot/launch/launch.py
+++ src/patrol_bot/launch/launch.py
@@ -48,8 +48,23 @@ def generate_launch_description():
                                 '-topic', 'robot_description'],
                         output='screen')
     
+    patrol_node = Node(
+        package='patrol_bot',
+        executable='patrol_node',
+        name='patrol_node',
+        output='screen',
+    )
+    
+    rotate_node = Node(
+        package='patrol_bot',
+        executable='rotate_node',
+        name='rotate_node',
+        output='screen',
+    )    
     
     return LaunchDescription([
+        patrol_node,
+        rotate_node,
         gazebo,
         robot_state_publisher,
         rviz_node,
