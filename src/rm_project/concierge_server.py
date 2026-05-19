914cc76784e7c4a215184fd513e40e9b7705cb4d
sharpiestealr <vicky.garcia.de.souza@gmail.com>
2026-05-19 20:17:06 -0300

    pj part 1

---
+++ src/rm_project/concierge_server.py
@@ -0,0 +1,120 @@
+from unittest import case
+
+import rclpy
+from rclpy.node import Node
+from std_srvs.srv import SetBool
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+from custom_interfaces.action import RotateAngle
+from custom_interfaces.action import GoToRoom
+import sys
+import time
+from rooms.yaml import rooms
+
+class ActionClient(Node):
+    def __init__(self):
+        super().__init__('projeto_srv_server')
+        self.srv_server = self.create_service(SetBool, 'busy', self.srv_callback)
+        self.subscription = self.create_subscription(LaserScan, 'scan', self.subscription_callback, 10)
+        self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.action_client = ActionClient(self, RotateAngle, 'rotate')
+        self.go_to_room_client = ActionClient(self, GoToRoom, 'go_to_room')
+
+    def srv_callback(self, request, response):
+        response.success = False
+        if (request.data):
+            response.success = True
+        return response
+    
+    def subscription_callback(self, msg):
+        start_time = time.perf_counter()
+        distancia = msg.range_min        
+        
+        if (distancia >= 0.5): 
+            while (time.perf_counter()-start_time > 2):
+                msg = Twist()
+                msg.angular.z = 0.5
+                self.publisher.publish(msg)
+            self.orientacao = msg.twist.twist.angular.z
+        
+        self.get_logger().info(f'Orientação: {self.orientacao:.3f}')
+        self.get_logger().info(f'Velocidade: {msg.angular.z:.2f}')
+
+        
+    def timer_callback(self):
+        msg = Twist()
+        msg.linear.x = 0
+        if (self.srv_server): msg.linear.x = 0.5
+        self.publisher.publish(msg)
+        self.get_logger().info(f'Velocidade: {msg.linear.x:.2f}')
+        
+    def send_goal(self):
+        self.get_logger().info('Sending goal...')
+        goal_msg = GoToRoom.Goal()
+        goal_msg.room_name = self.room
+
+        self.action_client.wait_for_server()
+
+        self.send_goal_future = self.action_client.send_goal_async(
+            goal_msg,
+            feedback_callback=self.feedback_callback
+        )
+        self.send_goal_future.add_done_callback(self.goal_response_callback)
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if self.room not in rooms:
+            self.get_logger().info('Goal rejected: sala inválida ou não existe')
+            return
+        elif self.busy == True:
+            self.get_logger().info('Goal rejected: já existe uma entrega em andamento')
+            return
+
+        self.get_logger().info('Goal accepted')
+        self.get_result_future = goal_handle.get_result_async()
+        self.get_result_future.add_done_callback(self.get_result_callback)
+
+    def feedback_callback(self, feedback_msg):
+        feedback = feedback_msg.feedback
+        self.get_logger().info(f'Fase: {feedback.phase}\nMetros até {self.room}: {feedback.distance_remaining}')
+        if feedback.distance_remaining > 0: self.busy = True
+
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info(f'Missão: {result.success} - {result.message}')
+        rclpy.shutdown()
+
+def main(args=None):
+    rclpy.init(args=args)
+    
+    estado = 'idle'
+    
+    match estado:
+        case 'idle':
+            node = ActionClient()
+            node.busy = False
+            estado = 'espera ordem'
+        case 'espera ordem':
+            node.room = rooms[input('Para onde vai o robô?')]
+            estado = 'anda frente'
+        case 'anda frente':
+             node.timer = node.create_timer(0.5, node.timer_callback)
+             node.feedback_callback()
+             if feedback.distance_remaining > 0: estado = 'verifica obstaculo'  
+             else: estado = 'idle'
+        case 'verifica obstaculo':
+            node.subscription = node.create_subscription(LaserScan, 'scan', node.subscription_callback, 10)
+            node.get_logger().info('Verificando obstáculos...')
+            node.feedback_callback()
+            estado = 'gira'
+        case 'gira':
+            node.action_client = ActionClient(node, RotateAngle, 'rotate')
+            node.send_goal()
+            node.feedback_callback()
+            estado = 'anda frente'     
+    
+    srv_server = SrvServer()
+    srv_server.send_goal(int(sys.argv[1]))
+    rclpy.spin(srv_server)
+    node.destroy_node()
+    rclpy.shutdown()
